038e2d54d6f12be898a752bac39a14085a0047ab
Sergey Edalov <edalov94@gmail.com>
2023-02-02 12:48:03 +0300

    Solution Homework_Seminar3

---
+++ Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Enter count: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int a = N / 10000;
+int b = N % 10;
+
+int c = N / 1000;
+int d = c % 10;
+
+int e = N / 10;
+int f = e % 10;
+
+if (a == b && d == f)
+{
+    Console.WriteLine($"число {N} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"число {N} не является палиндромом");
+}
+
+// Console.WriteLine(a);
+// Console.WriteLine(b);
+// Console.WriteLine(c);
+// Console.WriteLine(d);
+// Console.WriteLine(e);
+// Console.WriteLine(f);
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Точка a: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Точка b: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+double S = Math.Sqrt(Math.Pow((x1-x), 2) + Math.Pow((y1-y), 2) + Math.Pow((z1-z), 2));
+
+Console.WriteLine(Math.Round(S, 2));
